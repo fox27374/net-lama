c3f62c87470da224af78cb71ba4d60ab3fc82fc3
Daniel Kofler <fox27374@gmail.com>
2021-09-08 22:51:43 +0200

    added web config section

--- net-lama-ui/test.py
+++ net-lama-ui/test.py
@@ -7,10 +7,24 @@ import json
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    response = get("http://10.140.80.1:5000/api/v1/configs/SpeedTest")
-    data = response.json()
-    return render_template("rest.html", data=data)
+def routeIndex():
+    clients = get("http://10.140.80.1:5000/api/v1/clients/all")
+    clients = clients.json()
+    #print(clients[0]['clientType'])
+    return render_template("status.html", clients=clients)
+
+@app.route('/config')
+def routeConfig():
+    configList = {}
+    configs = get("http://10.140.80.1:5000/api/v1/configs/all")
+    configs = configs.json()
+
+    for config in configs['configs']:
+        appConfig = get("http://10.140.80.1:5000/api/v1/configs/" + config)
+        appConfig = appConfig.json()
+        configList.update(appConfig)
+
+    return render_template("config.html", configList=configList)
 
 if __name__ == "__main__":
     app.run(debug='True', host= '0.0.0.0', port=5500)
